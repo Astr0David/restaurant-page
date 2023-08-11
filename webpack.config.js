5db0ad8a42689b47bd28a7103c65acebbf4d003e
David Orimoyegun <dorimoyegun@outlook.com>
2023-08-11 13:04:21 +0100

    enabled css fucntionality

--- webpack.config.js
+++ webpack.config.js
@@ -21,4 +21,12 @@ module.exports = {
             title: 'Restaurant Page',
         }),
     ],
+    module: {
+        rules: [
+          {
+            test: /\.css$/i,
+            use: ['style-loader', 'css-loader'],
+          },
+        ],
+      },
 };
